6d8c9f3277a614fceb99388f459e1cd99b7406c2
samantharamsey <55068967+samantharamsey@users.noreply.github.com>
2019-12-06 14:25:23 -0500

    Add files via upload

--- mashed_potatoes.py
+++ mashed_potatoes.py
@@ -117,15 +117,20 @@ for i in gamma:
             e_min = (a_min - rp_min)/a_min
     
     
-    data = data.append(pd.DataFrame({'gamma (deg)': i, 
-                                     'v1 max': abs(v1_max),
+    data = data.append(pd.DataFrame({'Saturn fpa (deg)': i, 
+                                     'Saturn v1 max': abs(v1_max),
+                                     'Saturn v1x max': v1_max*np.sin(i*(np.pi/180)),
+                                     'Saturn v1y max': v1_max*np.cos(i*(np.pi/180)),
                                      'vp max': vp_max, 
                                      'rp max': rp_max, 
                                      'max semimajor axis': a_max,
                                      'max eccentricity': e_max, 
                                      'max energy': E_max,
                                      'max momentum': H_max,
-                                     'v1 min': abs(v1_min),
+                                     'Saturn fpa (deg)': i,
+                                     'Saturn v1 min': abs(v1_min),
+                                     'Saturn v1x min': v1_min*np.sin(i*(np.pi/180)),
+                                     'Saturn v1y min': v1_min*np.cos(i*(np.pi/180)),
                                      'vp min': vp_min, 
                                      'rp min': rp_min, 
                                      'min semimajor axis': a_min,
@@ -133,16 +138,14 @@ for i in gamma:
                                      'min energy': E_min,
                                      'min momentum': H_min},
                                      index = [0]), ignore_index = True)
-    
-#data.to_csv(r'C:\Users\saman\OneDrive\Desktop\senior_design\potato.csv', index = False)
 
 # wrt Saturn
-vx_max = data['v1 max']*np.sin(data['gamma (deg)']*(np.pi/180))
-vy_max = data['v1 max']*np.cos(data['gamma (deg)']*(np.pi/180))
+vx_max = data['Saturn v1 max']*np.sin(data['Saturn fpa (deg)']*(np.pi/180))
+vy_max = data['Saturn v1 max']*np.cos(data['Saturn fpa (deg)']*(np.pi/180))
 vz_max = [0]*len(vx_max)
 
-vx_min = data['v1 min']*np.sin(data['gamma (deg)']*(np.pi/180))
-vy_min = data['v1 min']*np.cos(data['gamma (deg)']*(np.pi/180))
+vx_min = data['Saturn v1 min']*np.sin(data['Saturn fpa (deg)']*(np.pi/180))
+vy_min = data['Saturn v1 min']*np.cos(data['Saturn fpa (deg)']*(np.pi/180))
 vz_min = [0]*len(vx_min)
 
 
@@ -162,64 +165,69 @@ for i in range(360):
     vmin_titan = np.linalg.norm([vx_min[i], new_vymin[i], vz_min[i]])
     vmin.append(vmin_titan)
 
-    
-data.insert(2, 'v1 max wrt Titan', vmax, True)
-data.insert(3, 'v1 min wrt Titan', vmin, True)
-
 titan_fpa_max = np.arctan2(vx_max, new_vymax)
 titan_fpa_min = np.arctan2(vx_min, new_vymin)
 
 data.insert(4, 'Titan max fpa', titan_fpa_max*(180/np.pi), True)
-data.insert(5, 'Titan min fpa', titan_fpa_min*(180/np.pi), True)
+data.insert(14, 'Titan min fpa', titan_fpa_min*(180/np.pi), True)
 
+data.insert(5, 'Titan v1 max magnitude', vmax, True)
+data.insert(6, 'Titan v1x max', vx_max, True)
+data.insert(7, 'Titan v1y max', new_vymax, True)
+
+data.insert(18, 'Titan v1 min magnitude', vmin, True)
+data.insert(19, 'Titan v1x min', vx_min, True)
+data.insert(20, 'Titan v1y min', new_vymin, True)
+
+data.to_csv(r'C:\Users\saman\OneDrive\Desktop\senior_design\potato.csv', index = False)
 
 f1 = plt.figure()
 ax1 = f1.add_subplot(111, polar = True)
 ax1.set_theta_zero_location("N")
 ax1.set_theta_direction(-1)
-ax1.plot(data['Titan max fpa'][:64]*(np.pi/180), data['v1 max wrt Titan'][:64], c = 'green')
-ax1.plot(data['Titan max fpa'][63:77]*(np.pi/180), data['v1 max wrt Titan'][63:77], c = 'orange')
-ax1.plot(data['Titan min fpa'][:76]*(np.pi/180), data['v1 min wrt Titan'][:76], c = 'blue')
-ax1.plot(data['Titan max fpa'][76:104]*(np.pi/180), data['v1 max wrt Titan'][76:104], c = 'red')
-ax1.plot(data['Titan min fpa'][75:104]*(np.pi/180), data['v1 min wrt Titan'][75:104], c = 'red')
-ax1.plot(data['Titan max fpa'][103:116]*(np.pi/180), data['v1 max wrt Titan'][103:116], c = 'orange')
-ax1.plot(data['Titan max fpa'][115:244]*(np.pi/180), data['v1 max wrt Titan'][115:244], c = 'green')
-ax1.plot(data['Titan min fpa'][103:256]*(np.pi/180), data['v1 min wrt Titan'][103:256], c = 'blue')
-ax1.plot(data['Titan max fpa'][243:257]*(np.pi/180), data['v1 max wrt Titan'][243:257], c = 'orange')
-ax1.plot(data['Titan max fpa'][256:284]*(np.pi/180), data['v1 max wrt Titan'][256:284], c = 'red')
-ax1.plot(data['Titan min fpa'][255:284]*(np.pi/180), data['v1 min wrt Titan'][255:284], c = 'red')
-ax1.plot(data['Titan max fpa'][283:296]*(np.pi/180), data['v1 max wrt Titan'][283:296], c = 'orange')
-ax1.plot(data['Titan max fpa'][295:]*(np.pi/180), data['v1 max wrt Titan'][295:], c = 'green')
-ax1.plot(data['Titan min fpa'][283:]*(np.pi/180), data['v1 min wrt Titan'][283:], c = 'blue')
-#ax1.fill_between(data['Titan max fpa'][::]*(np.pi/180), data['v1 max wrt Titan'][::], 
-#                     data['v1 min wrt Titan'][::], facecolor='green', alpha=0.2)
+ax1.plot(data['Titan max fpa'][:64]*(np.pi/180), data['Titan v1 max magnitude'][:64], c = 'green')
+ax1.plot(data['Titan max fpa'][63:77]*(np.pi/180), data['Titan v1 max magnitude'][63:77], c = 'orange')
+ax1.plot(data['Titan min fpa'][:76]*(np.pi/180), data['Titan v1 min magnitude'][:76], c = 'blue')
+ax1.plot(data['Titan max fpa'][76:104]*(np.pi/180), data['Titan v1 max magnitude'][76:104], c = 'red')
+ax1.plot(data['Titan min fpa'][75:104]*(np.pi/180), data['Titan v1 min magnitude'][75:104], c = 'red')
+ax1.plot(data['Titan max fpa'][103:116]*(np.pi/180), data['Titan v1 max magnitude'][103:116], c = 'orange')
+ax1.plot(data['Titan max fpa'][115:244]*(np.pi/180), data['Titan v1 max magnitude'][115:244], c = 'green')
+ax1.plot(data['Titan min fpa'][103:256]*(np.pi/180), data['Titan v1 min magnitude'][103:256], c = 'blue')
+ax1.plot(data['Titan max fpa'][243:257]*(np.pi/180), data['Titan v1 max magnitude'][243:257], c = 'orange')
+ax1.plot(data['Titan max fpa'][256:284]*(np.pi/180), data['Titan v1 max magnitude'][256:284], c = 'red')
+ax1.plot(data['Titan min fpa'][255:284]*(np.pi/180), data['Titan v1 min magnitude'][255:284], c = 'red')
+ax1.plot(data['Titan max fpa'][283:296]*(np.pi/180), data['Titan v1 max magnitude'][283:296], c = 'orange')
+ax1.plot(data['Titan max fpa'][295:]*(np.pi/180), data['Titan v1 max magnitude'][295:], c = 'green')
+ax1.plot(data['Titan min fpa'][283:]*(np.pi/180), data['Titan v1 min magnitude'][283:], c = 'blue')
+ax1.fill(data['Titan max fpa'][::]*(np.pi/180), data['Titan v1 max magnitude'][::], 'darkseagreen',
+         data['Titan min fpa'][::]*(np.pi/180), data['Titan v1 min magnitude'][::], 'white')
 plt.title('Family of v1 Velocity Vectors wrt Titan')
 plt.legend(['Maximum Allowable v1 Value', 
             'Maximum Allowable v1 Value - Hitting Escape Velocity Constraint',
             'Minimum Allowable v1 Value', 
-            'Bad Trajectories - Perigee Radius Smaller than Saturns Radius'], loc = 8)
+            'Bad Trajectories - Perigee Radius Smaller than Saturns Radius'])
  
 f2 = plt.figure()
 ax2 = f2.add_subplot(111, polar = True)
 ax2.set_theta_zero_location("N")
 ax2.set_theta_direction(-1)  
-ax2.plot(data['gamma (deg)'][:64]*(np.pi/180), data['v1 max'][:64], c = 'green')
-ax2.plot(data['gamma (deg)'][63:77]*(np.pi/180), data['v1 max'][63:77], c = 'orange')
-ax2.plot(data['gamma (deg)'][76:104]*(np.pi/180), data['v1 max'][76:104], c = 'red')
-ax2.plot(data['gamma (deg)'][:76]*(np.pi/180), data['v1 min'][:76], c = 'blue')
-ax2.plot(data['gamma (deg)'][103:116]*(np.pi/180), data['v1 max'][103:116], c = 'orange')
-ax2.plot(data['gamma (deg)'][115:244]*(np.pi/180), data['v1 max'][115:244], c = 'green')
-ax2.plot(data['gamma (deg)'][243:257]*(np.pi/180), data['v1 max'][243:257], c = 'orange')
-ax2.plot(data['gamma (deg)'][256:284]*(np.pi/180), data['v1 max'][256:284], c = 'red')
-ax2.plot(data['gamma (deg)'][283:296]*(np.pi/180), data['v1 max'][283:296], c = 'orange')
-ax2.plot(data['gamma (deg)'][295:]*(np.pi/180), data['v1 max'][295:], c = 'green')
-ax2.plot(data['gamma (deg)'][:76]*(np.pi/180), data['v1 min'][:76], c = 'blue')
-ax2.plot(data['gamma (deg)'][75:104]*(np.pi/180), data['v1 min'][75:104], c = 'red')
-ax2.plot(data['gamma (deg)'][103:256]*(np.pi/180), data['v1 min'][103:256], c = 'blue')
-ax2.plot(data['gamma (deg)'][255:284]*(np.pi/180), data['v1 min'][255:284], c = 'red')
-ax2.plot(data['gamma (deg)'][283:]*(np.pi/180), data['v1 min'][283:], c = 'blue')
-ax2.fill_between(data['gamma (deg)'][::]*(np.pi/180), data['v1 max'][::], 
-                     data['v1 min'][::], facecolor='green', alpha=0.2)
+ax2.plot(data['Saturn fpa (deg)'][:64]*(np.pi/180), data['Saturn v1 max'][:64], c = 'green')
+ax2.plot(data['Saturn fpa (deg)'][63:77]*(np.pi/180), data['Saturn v1 max'][63:77], c = 'orange')
+ax2.plot(data['Saturn fpa (deg)'][76:104]*(np.pi/180), data['Saturn v1 max'][76:104], c = 'red')
+ax2.plot(data['Saturn fpa (deg)'][:76]*(np.pi/180), data['Saturn v1 min'][:76], c = 'blue')
+ax2.plot(data['Saturn fpa (deg)'][103:116]*(np.pi/180), data['Saturn v1 max'][103:116], c = 'orange')
+ax2.plot(data['Saturn fpa (deg)'][115:244]*(np.pi/180), data['Saturn v1 max'][115:244], c = 'green')
+ax2.plot(data['Saturn fpa (deg)'][243:257]*(np.pi/180), data['Saturn v1 max'][243:257], c = 'orange')
+ax2.plot(data['Saturn fpa (deg)'][256:284]*(np.pi/180), data['Saturn v1 max'][256:284], c = 'red')
+ax2.plot(data['Saturn fpa (deg)'][283:296]*(np.pi/180), data['Saturn v1 max'][283:296], c = 'orange')
+ax2.plot(data['Saturn fpa (deg)'][295:]*(np.pi/180), data['Saturn v1 max'][295:], c = 'green')
+ax2.plot(data['Saturn fpa (deg)'][:76]*(np.pi/180), data['Saturn v1 min'][:76], c = 'blue')
+ax2.plot(data['Saturn fpa (deg)'][75:104]*(np.pi/180), data['Saturn v1 min'][75:104], c = 'red')
+ax2.plot(data['Saturn fpa (deg)'][103:256]*(np.pi/180), data['Saturn v1 min'][103:256], c = 'blue')
+ax2.plot(data['Saturn fpa (deg)'][255:284]*(np.pi/180), data['Saturn v1 min'][255:284], c = 'red')
+ax2.plot(data['Saturn fpa (deg)'][283:]*(np.pi/180), data['Saturn v1 min'][283:], c = 'blue')
+ax2.fill_between(data['Saturn fpa (deg)'][::]*(np.pi/180), data['Saturn v1 max'][::], 
+                     data['Saturn v1 min'][::], facecolor='darkseagreen')
 plt.title('Family of v1 Velocity Vectors wrt Saturn')
 plt.legend(['Good Trajectories - r_p about equal to r_enceladus', 
             'OK Trajectories - Hitting Escape Velocity Constraint', 
