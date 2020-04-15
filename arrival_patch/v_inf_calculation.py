a15fa492b6550d0bcdd23939c91a19d30315ff28
Samantha Ramsey <sramse18@vols.utk.edu>
2020-04-15 11:31:50 -0400

    updates

--- arrival_patch/v_inf_calculation.py
+++ arrival_patch/v_inf_calculation.py
@@ -69,29 +69,38 @@ def calc_vinf_titan(v_inf, dec, intercept):
     # v_infinity magnitude
     v_inf_mag = np.sqrt(v_infx**2 + v_infy**2 + v_infz**2)
     
-    return v_infx, v_infy, v_infz, v_inf_mag
+    # delination
+    declination = np.arctan2(v_infz, np.sqrt(v_infx**2 + v_infy**2))*(180/np.pi)
+    
+    return v_infx, v_infy, v_infz, v_inf_mag, declination
 
 
 if __name__ == '__main__':
     
-    filepath = r'C:\Spice_Kernels'
-    filename = r'\10_declination'
+    filepath = r'C:\Users\saman\OneDrive\Desktop\senior_design\arrival_patch'
+    filename = r'\mane_jun15_case'
     data = pd.DataFrame([])
     v_infx = []
     v_infy = []
     v_infz = []
+    v_inf_mag = []
+    declination = []
+    
     for i in range(360):
-        resultx, resulty, resultz, resultmag = calc_vinf_titan(6, 10, i)
+        resultx, resulty, resultz, resultmag, resultdec = calc_vinf_titan(5.7622, 14.2513, i)
         v_infx.append(resultx)
         v_infy.append(resulty)
         v_infz.append(resultz)
+        v_inf_mag.append(resultmag)
+        declination.append(resultdec)
        
         data = data.append(pd.DataFrame({'intercept location (deg)': i,
                                          'v_infinity wrt Titian magnitude (km/s)': resultmag,
+                                         'declination': resultdec,
                                          'v_infinity x component': resultx,
                                          'v_infinity y component': resulty,
                                          'v_infinity z component': resultz},
-                                         index=[0]), ignore_index=True)
+                                          index=[0]), ignore_index=True)
     # send results to excel
     data.to_csv(filepath + filename + r'.csv', index = False)
     # send results to HDF5 - faster loading
@@ -99,11 +108,17 @@ if __name__ == '__main__':
     
     # plot stuff
     fig1 = plt.figure()
-    plt.plot(v_infx)
-    plt.plot(v_infy)
-    plt.plot(v_infz)
-    plt.ylabel('velocity (km/s)')
-    plt.xlabel('intercept location (deg)')
-    plt.legend(['x component', 'y component', 'z component'])
-    plt.title('V infinity wrt Titan: v_inf wrt Saturn = 6, dec = 10')
+    ax = fig1.add_subplot(111)
+    ax.plot(v_infx)
+    ax.plot(v_infy)
+    ax.plot(v_infz)
+    ax.plot(v_inf_mag)
+    ax2 = ax.twinx()
+    ax2.plot(declination, c = 'purple')
+    ax.set_ylabel('velocity (km/s)')
+    ax2.set_ylabel('declination (deg)')
+    ax.set_xlabel('intercept location (deg)')
+    ax.legend(['x component', 'y component', 'z component', 'v magnitude'], loc = 7)
+    ax2.legend(['declination'])
+    plt.title('V infinity wrt Titan: MAnE June 15 case')
     plt.show()
